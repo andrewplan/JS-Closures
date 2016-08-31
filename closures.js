eeb458fb37b0b65e003b28205dcc1c4ee3335202
andrewplan <a.plan88@gmail.com>
2016-08-30 22:06:11 -0500

    Finished 6/8 problems, stumped on problem 7.  Ask for help if I don't understand after lecture.

--- closures.js
+++ closures.js
@@ -12,12 +12,13 @@ var outer = function(){
 // Invoke outer saving the return value into another variable called 'inner'.
 
 // Code Here
+var inner = outer();
 
 
 //Once you do that, invoke inner.
 
   //Code Here
-
+  inner();
 
 
 //////////////////PROBLEM 2////////////////////
@@ -36,7 +37,9 @@ var callFriend = function(){
 // Create a makeCall function that when invoked logs  'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
-
+var makeCall = function() {
+  return callFriend( '435-215-9248' );
+}
 
 
 
@@ -52,13 +55,19 @@ var callFriend = function(){
 */
 
 //Code Here
+var makeCounter = function() {
+  var count = 0;
+  return function() {
+    return ++count;
+  }
+};
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -74,10 +83,15 @@ var callFriend = function(){
 function counterFactory(value) {
 
   // Code here.
-
-
   return {
-  }
+    inc: function() {
+      return ++value;
+    }
+    , dec: function() {
+      return --value;
+    }
+  };
+
 }
 
 
@@ -96,11 +110,13 @@ counter = counterFactory(10);
     var welcomeText = 'You\'re doing awesome, keep it up ';
 
     // code message function here.
-
+    function message() {
+      return welcomeText + firstname + ' ' + lastname + '.';
+    }
 
     //Uncommment this to return the value of your invoked message function
 
-    //return message()
+    return message();
   }
 
   motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
@@ -127,12 +143,17 @@ counter = counterFactory(10);
 
     return {
       // Code here.
+      publicMethod: function() {
+        return privateMethod();
+      }
+
     };
 
   })();
 
+
 //Uncomment this after you create your public method
-//   module.publicMethod();
+  module.publicMethod();
 
 
 
@@ -143,13 +164,13 @@ counter = counterFactory(10);
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000)
+      setTimeout(function() {
+        console.log(i);
+      }, i * 1000);
   }
 
   function newScope(i) {
-    console.log(i)
+    console.log(i);
   }
 }
 timeOutCounter();
@@ -162,6 +183,7 @@ timeOutCounter();
 //////////////////PROBLEM 8////////////////////
 
 var funcArray = [];
+
 
 /*
   Make the following code work
